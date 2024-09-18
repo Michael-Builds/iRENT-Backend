95dfc43c6fe58c1f655938e2cdcc5e7c6c9d1b24
Michael-Builds <michaelkpantramp@gmail.com>
2024-09-18 13:58:19 +0000

    Updated

--- app.js
+++ app.js
@@ -15,18 +15,9 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-
-        if (ORIGIN.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: 'https://i-rent-frontend.vercel.app',
     credentials: true
-}));
+}))
 
 // routes
 app.use("/auth/api", userRouter);
